2c80af447b9d9f0cb454e7d91f90b9721cdb7f0e
Cordtus <Cordtus@gmail.com>
2025-06-15 10:26:05 -0600

    remove wrongly committed reference files

--- context/precompiles/distribution/errors.go
+++
@@ -1,8 +0,0 @@
-package distribution
-
-const (
-	// ErrDifferentValidator is raised when the origin address is not the same as the validator address.
-	ErrDifferentValidator = "origin address %s is not the same as validator address %s"
-	// ErrInvalidAmount is raised when the given sdk coins amount is invalid
-	ErrInvalidAmount = "invalid amount %s"
-)
